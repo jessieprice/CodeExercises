b4e99f33e1b44cdff0b9af75715ec97170b6cdfc
Jessie Price <jprice@shadow-soft.com>
2019-10-03 16:05:33 -0400

    optimized two sum to O(n)

--- .java/twosum.java
+++ .java/twosum.java
@@ -1,4 +1,6 @@
 Class Solution {
+ 
+    // less efficient O(n^2)
     public int[] twoSum(int[] nums, int target) {
         boolean not_found = true;
         int[] answer = new int[2];
@@ -12,6 +14,26 @@ Class Solution {
                     }
                 }
             } 
+        }
+        return answer;
+    }
+
+    // more efficient O(n)
+    public int[] twoSum(int[] nums, int target) {
+        // will be <value of int, index of int>
+        Map<Integer, Integer> mapped = new HashMap<>();
+        int[] answer = new int[2];
+        
+        for (int i = 0 ; i < nums.length; i ++ ) {
+            // compute remainder needed to reach sum
+            int complement = target - nums[i];
+            if (mapped.containsKey(complement)) {
+                // return array {index of complement, index og}
+                answer[0] = mapped.get(complement);
+                answer[1] = i;
+            }
+            // if complement not in the map
+            mapped.put(nums[i], i);
         }
         return answer;
     }
